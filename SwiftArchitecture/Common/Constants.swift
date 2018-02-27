818aea2ad7f2a95304717c2f9b56b366d2271142
Yao Haitao <yao_ht@me.com>
2018-02-27 15:52:21 +0900

    添加常量存放的位置 Constants.swift

---
+++ SwiftArchitecture/Common/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  SwiftArchitecture
+//
+//  Created by 姚海涛 on 2018/2/27.
+//  Copyright © 2018年 yaohaitao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - 配置信息
+
+enum Config {
+    static let baseURL = NSURL(string: "https://localhost")
+}
+
+// MARK: - 颜色信息
+
+enum Color {
+    static let primaryColor = UIColor.black
+}
